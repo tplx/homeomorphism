aacf4593254d535f8d0c691e58b5d497b0b0b40b
technicca <cake3327@gmail.com>
2024-03-13 11:51:27 +0300

    add docs

---
+++ docs/theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Homeomorphism.cpp docs</span>,
+  project: {
+    link: 'https://github.com/u66u/homeomorphism',
+  },
+  docsRepositoryBase: 'https://github.com/u66u/homeomorphism/docs',
+  footer: {
+    text: 'Homeomorphism.cpp docs',
+  },
+}
+
+export default config
